1b0088dba0d29c40150b3d17f763159cf6df4065
lajoh20 <50496644+lajoh20@users.noreply.github.com>
2020-02-27 13:17:05 -0600

    Update Day004.py

--- daily_code_problem/Day004.py
+++ daily_code_problem/Day004.py
@@ -7,3 +7,16 @@ For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should g
 
 You can modify the input array in-place.
 """
+missing_int_found = False
+input_arr = [3, 4, -1,1]
+if 1  not in input_arr:
+	print(1)
+else:
+	input_arr.sort()
+	index_point = input_arr.index(1)
+	while missing_int_found == False:
+		if (input_arr[index_point-1]) > input_arr[index_point]:
+			index_point = index_point + 1
+		else:
+			print((input_arr[index_point])+1)
+			missing_int_found = True
